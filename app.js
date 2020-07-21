175fdf0a727144d200a0e829d4651ba11848db7d
shashank <Shashank.Satpute@sigma-systems.com>
2020-07-21 16:09:28 +0530

    Cross Origin header

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/empList', usersRouter);
 app.use(express.static(path.join(__dirname, 'build')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
